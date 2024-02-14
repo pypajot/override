d0cc2fee15c97fd74b6b1a5e64012f38a0cb457b
ppajot <ppajot@student.42.fr>
2024-02-14 19:27:59 +0100

    level06

---
+++ level06/get_auth.c
@@ -0,0 +1,14 @@
+int main(){
+	char s[7] = "aaaaaa";
+	int v4;
+
+	v4 = (s[3] ^ 0x1337) + 6221293;
+    for (int i = 0; i < 6; ++i )
+    {
+      if ( s[i] <= 31 )
+        return 1;
+      v4 += (v4 ^ (unsigned int)s[i]) % 0x539;
+    }
+		printf("%i\n", v4);
+    return 0;
+}
